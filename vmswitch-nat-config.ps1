513d3dfd54accf3854dc97455ac99172b2028046
Artyom Lyubimov <yaadex@yandex.ru>
2020-10-18 14:45:38 +0300

    On branch master  Changes to be committed: 	modified:   vmswitch-nat-config.ps1

---
+++ vmswitch-nat-config.ps1
@@ -0,0 +1,3 @@
+New-VMSwitch -Name "NATSwitch" -SwitchType Internal
+New-NetIPAddress -IPAddress 192.168.8.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch)"
+New-NetNat -Name "NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.8.0/24
