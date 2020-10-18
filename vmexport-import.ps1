3cdaf4e2860fa3004889424d2ba09a7560808946
Artyom Lyubimov <yaadex@yandex.ru>
2020-10-18 16:08:55 +0300

    modified:   vmexport-import.ps1 	modified:   vmswitch-nat-config.ps1

--- vmexport-import.ps1
+++ vmexport-import.ps1
@@ -1,1 +1,11 @@
+Import-Module Hyper-V
 
+$VMName = "VM1"
+$ExportPath = "D:\VM_IMAGE\$VMName"
+$NewVMName = "VM2"
+$VMDir = "D:\VM\"
+
+Export-VM -Name $VMName -Path $ExportPath
+Import-VM -Path 'D:\ate\images\ate-windows-base\Virtual Machines\2005C276-155C-4AD1-8DB6-963EDFB6DB84.vmcx' -Copy -GenerateNewId -VirtualMachinePath D:\working-images\img001 -VhdDestinationPath 'D:\working-images\img001\Virtual Hard Disks'
+$VM = Get-VM | Where-Object {$_.Path.StartsWith('D:\working-images\img001')}
+Rename-VM -VM $VM -NewName img001
--- vmswitch-nat-config.ps1
+++ vmswitch-nat-config.ps1
@@ -1,3 +1,5 @@
+Import-Module Hyper-V
+
 New-VMSwitch -Name "NATSwitch" -SwitchType Internal
 New-NetIPAddress -IPAddress 192.168.8.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch)"
 New-NetNat -Name "NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.8.0/24
